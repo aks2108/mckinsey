44b24ea1a9ee41dfe74fd4422161f9e5e10fc6c8
aks2108 <kumar.akshat87@gmail.com>
2013-02-28 03:53:44 -0500

    Update question_1__list_of_urls_tweets.py

--- question_1__list_of_urls_tweets.py
+++ question_1__list_of_urls_tweets.py
@@ -3,16 +3,18 @@
 # line. The app should accept a hashtag as an argument and should only print out 
 # unique urls found in the 100 most recent tweets that matched the hashtag.  
 #
-# Solution Approach : This question is solved using twitter_text library which 
-# gives entities from a twitter text. This program first takes a hash tag from the
-# user by command prompt and then using the same searches first 100 recent tweets and 
-# stores into a list . This is then operated on extractor class to get urls. The given
-# list of urls are not unique which will be stored again into a set which gives a unique 
-# set of urls.
+# Solution Approach : This question is solved using by two approaches 
+# 1. Using twitter_text library which gives entities from a twitter text.
+# 2. Using Regular expression operations
+# This program first takes a hash tag from the user by command prompt and then using the
+# same, searches first 100 recent tweets and stores into a list . Then the text of the 
+# tweets is operated on the 'findall' function corrosponding to a pattern which peculiar
+# to urls which gives a list of urls present in the tweets. The list of urls stored 
+# again into a set which gives a unique  set of urls.
 ##########################################################################################
 
 
-# importing twitter libraries 
+# import twitter libraries 
 # twitter_text library is used for auto-converting URLs, 
 # mentions, hashtags, lists, etc. in Twitter text. 
 
@@ -20,13 +22,14 @@ import twitter
 import json
 import sys
 import twitter_text
+import re
 
 # declaring all the variables
 
 list_of_urls_in_tweets = []       # Variable to store list of unique tweets
 i=1                               # Counter for printing
 search_results = []               # Variable to store the twitter search respons
-tweet_text = []                   # Variable to store the tweets
+                 
 
 # take hash tag term from command prompt
 
@@ -43,20 +46,14 @@ twitter_search = twitter.Twitter(domain="search.twitter.com")
 
 search_results=twitter_search.search(q=term, count=100, rpp=100, result_type='recent', include_entities=1)                                               
 
-# Storing all the tweets and making a twitter_text class 
+# Searching the url pattern in the tweet text
 
 for text in search_results['results']:
-  tweet_text.append(text['text'])       
-
-extractor = twitter_text.Extractor(tweet_text) 	
-
-# Getting urls from the tweet text object 
-
-for url in extractor.extract_urls():
-  list_of_urls_in_tweets.append(url)
+  list_of_urls_in_tweets.extend(re.findall('(https?://\S+)', text['text']))       
   
 # Storing in a set to store unique urls
 
+number_of_urls=len(list_of_urls_in_tweets)
 list_of_urls_in_tweets=list(set(list_of_urls_in_tweets))                                 
 
 for url in list_of_urls_in_tweets:
@@ -64,5 +61,5 @@ for url in list_of_urls_in_tweets:
  i+=1
 
 print "Total number of tweets : ",len(search_results['results'])
-print "Total number of list_of_urls_in_tweets : ",len(extractor.extract_urls())
+print "Total number of list_of_urls_in_tweets : ",number_of_urls
 print "Total number of unique list_of_urls_in_tweets : ",len(list_of_urls_in_tweets)
